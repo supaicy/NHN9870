f1673d8daed111223b202dc3cca748279c77d8b0
supaicy <iwilldtw@gmail.com>
2023-10-12 20:54:01 +0900

    CPU 커밋합니다.
    manageResource 의 retrun type을 String 으로 하는게 좋을 듯 합니다..?

--- NHN9870/src/main/java/com/nhnacademy/manageResource/Cpu.java
+++ NHN9870/src/main/java/com/nhnacademy/manageResource/Cpu.java
@@ -1,5 +1,18 @@
 package com.nhnacademy.manageResource;
 
+import com.sun.management.OperatingSystemMXBean;
+
+import java.lang.management.ManagementFactory;
+
 public class Cpu {
 
+    public static void main(String[] args) {
+        Cpu a = new Cpu();
+        System.out.println(a.getCpu());
+    }
+
+    public double getCpu() {
+        OperatingSystemMXBean fuck = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
+        return fuck.getSystemCpuLoad() * 100;
+    }
 }
