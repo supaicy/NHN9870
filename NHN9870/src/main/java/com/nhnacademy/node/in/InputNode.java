be626bdfc9a134b6f9d0598714ce5ff49e4e9294
ckddms6530 <ckddms6530@gmail.com>
2023-10-12 18:27:35 +0900

    exeption 및 InputNode 구현

--- NHN9870/src/main/java/com/nhnacademy/node/in/InputNode.java
+++ NHN9870/src/main/java/com/nhnacademy/node/in/InputNode.java
@@ -10,7 +10,7 @@ import lombok.extern.log4j.Log4j2;
 
 
 @Log4j2
-public class InputNode extends ActiveNode{
+public class InputNode extends ActiveNode {
     Wire[] outputWires;
 
     InputNode(int count) {
@@ -47,6 +47,9 @@ public class InputNode extends ActiveNode{
         return outputWires[index];
     }
 
+    public int getOutputWireCount() {
+        return outputWires.length;
+    }
 
     void output(Message message) {
         log.trace("Message Out");
