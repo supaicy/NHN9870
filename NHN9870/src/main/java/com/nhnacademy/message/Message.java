b816d08f8904ea7760564550dd20bf1c8a5cb3b0
supaicy <iwilldtw@gmail.com>
2023-10-12 17:56:01 +0900

    Message 클래스 구현1

--- NHN9870/src/main/java/com/nhnacademy/message/Message.java
+++ NHN9870/src/main/java/com/nhnacademy/message/Message.java
@@ -1,4 +1,24 @@
 package com.nhnacademy.message;
 
 public class Message {
+    static int count = 0;
+    final String id;
+    long creationTime;
+
+    Message() {
+        creationTime = System.currentTimeMillis();
+        id = String.valueOf(creationTime) + (++count);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public long getCreationTime() {
+        return creationTime;
+    }
+
+    public static int getCount() {
+        return count;
+    }
 }
