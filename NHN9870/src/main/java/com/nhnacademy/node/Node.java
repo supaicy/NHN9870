fb86dc459c7697378ebbf01713a937f3f33df7b9
supaicy <iwilldtw@gmail.com>
2023-10-12 16:44:20 +0900

    Node

--- NHN9870/src/main/java/com/nhnacademy/node/Node.java
+++ NHN9870/src/main/java/com/nhnacademy/node/Node.java
@@ -1,4 +1,19 @@
 package com.nhnacademy.node;
 
 public class Node {
+    String id;
+    static int count;
+
+    Node(String id) {
+        this.id = id;
+    }
+
+    Node() {
+        this(String.valueOf(System.currentTimeMillis()) + (++count));
+    }
+
+    public int getCount() {
+        return count;
+    }
+
 }
