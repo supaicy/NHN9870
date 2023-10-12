afb7af896f1166b899c4dd5ee5913bc6f9763670
ckddms6530 <ckddms6530@gmail.com>
2023-10-12 16:43:40 +0900

    Wire 클래스 구현

--- NHN9870/src/main/java/com/nhnacademy/wire/Wire.java
+++ NHN9870/src/main/java/com/nhnacademy/wire/Wire.java
@@ -1,4 +1,25 @@
 package com.nhnacademy.wire;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import com.nhnacademy.message.Message;
+
 public class Wire {
+        Queue<Message> messageQueue;
+
+    public Wire() {
+        messageQueue = new LinkedList<>();
+    }
+
+    public void put(Message message) {
+        messageQueue.add(message);
+    }
+
+    public boolean hasMessage() {
+        return !messageQueue.isEmpty();
+    }
+
+    public Message get() {
+        return messageQueue.poll();
+    }
 }
