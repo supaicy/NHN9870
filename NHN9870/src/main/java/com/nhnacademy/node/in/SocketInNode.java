d99c4f96f8abfb79e24903f960b67fb9cc34fb2d
sylim <sammys27@naver.com>
2023-10-13 15:06:49 +0900

    SocketInNode 구현

--- NHN9870/src/main/java/com/nhnacademy/node/in/SocketInNode.java
+++ NHN9870/src/main/java/com/nhnacademy/node/in/SocketInNode.java
@@ -1,31 +1,45 @@
 package com.nhnacademy.node.in;
 
-import com.nhnacademy.message.StringMessage;
-
-import java.io.BufferedReader;
+import com.nhnacademy.message.TCPRequestMessage;
+import java.io.BufferedInputStream;
 import java.io.IOException;
 
 public class SocketInNode extends InputNode {
-    BufferedReader reader;
 
-    public SocketInNode(int count, BufferedReader reader){
+    BufferedInputStream inputStream;
+    byte[] buffer;
+
+    public SocketInNode(int count, BufferedInputStream inputStream){
         super(count);
-        this.reader = reader;
+        this.inputStream = inputStream;
     }
 
     @Override
-    protected void preprocess(){}
+    protected void preprocess(){
+        buffer = new byte[100000];
+    }
 
     @Override
     protected void main(){
-        String line = null;
-        try {
-            line = reader.readLine();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        int length = 0;
+        while(!Thread.currentThread().isInterrupted()){
+            try {
+                length = inputStream.read(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
-        StringMessage message = new StringMessage(line);
+
+        TCPRequestMessage message = new TCPRequestMessage(this.getId(),buffer,length);
         output(message);
     }
 
+    @Override
+    protected void postprocess(){
+        try {
+            inputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
