738cf3217d8cca67489109689bf0fc9e93a20961
supaicy <iwilldtw@gmail.com>
2023-10-13 13:51:34 +0900

    Server 수정 UML 보면서 만들어용

--- NHN9870/src/main/java/com/nhnacademy/node/inout/Server.java
+++ NHN9870/src/main/java/com/nhnacademy/node/inout/Server.java
@@ -1,14 +1,21 @@
 package com.nhnacademy.node.inout;
 
+import com.nhnacademy.message.Message;
+import com.nhnacademy.message.TCPRequestMessage;
+import com.nhnacademy.message.TCPResponseMessage;
 import lombok.Getter;
+import lombok.extern.log4j.Log4j2;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
-
+@Log4j2
 public class Server extends InOutNode{
     static class Handler implements Runnable {
         @Getter
@@ -69,5 +76,63 @@ public class Server extends InOutNode{
         }
     }
 
+    int port = 1234;
+    ServerSocket serverSocket;
+    Map<String, Handler> handlerMap;
+    Thread receiverThread;
 
+    public Server(String id) {
+        super(id, 1, 1);
+        handlerMap = new HashMap<>();
+    }
+
+    Handler getHandler(String id) {
+        return handlerMap.get(id);
+    }
+
+    void preprocess() {
+        try {
+            serverSocket = new ServerSocket(port);
+            receiverThread = new Thread(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    try {
+                        for (int i = 0; i < getInputWireCount(); i++) {
+                            if((getInputWire(i) != null) && getInputWire(i).hasMessage()) {
+                                Message message = getInputWire(i).get();
+                                if (message instanceof TCPResponseMessage) {
+                                    TCPResponseMessage responseMessage = (TCPResponseMessage) message;
+                                    Handler handler = getHandler(responseMessage.getSenderId());
+
+                                    handler.write(responseMessage.getPayload());
+                                }
+                            }
+                        }
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            });
+            receiverThread.start();
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            stop();
+        }
+    }
+
+    void process() {
+        try {
+            Socket socket = serverSocket.accept();
+            Handler handler = new Handler(socket, this);
+            handler.setCallback((data, length) -> {
+                output(new TCPRequestMessage(handler.getId(), data, length));
+            });
+            handler.start();
+            handlerMap.put(handler.getId(), handler);
+
+        } catch (IOException e) {
+            log.error(e.getMessage());
+            stop();
+        }
+    }
 }
